4448ffd549036ba3647090e014b65bdd7440bf96
Alazard Célian <celian.alazard@gmail.com>
2023-08-07 02:12:03 +0200

    test_01

--- index.js
+++ index.js
@@ -23,28 +23,45 @@ function httpGet(theUrl)
     return xmlHttp.responseText;
 }
 
+var jsonhouse
+
 var connectingIndicator = document.getElementById('connectIndicator')
 var errIndicator = document.getElementById('errConnect')
 var notConnectIndicator = document.getElementById('notConnected')
 var connectIndicator = document.getElementById('connected')
+
+var serveraddress
 
 setElementVisibility(errIndicator, false)
 setElementVisibility(connectIndicator, false)
 setElementVisibility(connectingIndicator, false)
 
 async function promptConnect(){
+    serveraddress = prompt("Enter the server's address:", "127.0.0.1")
     setElementVisibility(connectingIndicator, true)
     setElementVisibility(notConnectIndicator, false)
     setElementVisibility(connectIndicator, false)
-    setElementVisibility(errIndicatorIndicator, false)
-    let response = await httpGet('http://192.168.1.62:3000/ping')
-    if (response == "pong"){
-        setElementVisibility(connectingIndicator, true)
+    setElementVisibility(errIndicator, false)
+    try {
+        let response = await httpGet('http://' + serveraddress + ':3000/ping')
+        if (response == "pong"){
+            setElementVisibility(connectingIndicator, false)
+            setElementVisibility(notConnectIndicator, false)
+            setElementVisibility(errIndicator, false)
+            connectIndicator.textContent = "fetching..."
+            setElementVisibility(connectIndicator, true)
+            let response_houseinfo = await httpGet('http://' + serveraddress + ':3000/houseInformation')
+            jsonhouse = JSON.parse(response_houseinfo)
+            connectIndicator.textContent = "connected to " + jsonhouse['servername'];
+        }else{
+            setElementVisibility(connectingIndicator, false)
+            setElementVisibility(notConnectIndicator, false)
+            setElementVisibility(errIndicator, true)
+        }  
+    } catch (error) {
+        setElementVisibility(connectingIndicator, false)
         setElementVisibility(notConnectIndicator, false)
-        setElementVisibility(errIndicatorIndicator, false)
-        connectIndicator.textContent = "connected to 192.168.1.62"
-        setElementVisibility(connectIndicator, true)
-    }else{
-
+        setElementVisibility(errIndicator, true)
+        console.log('[+] ' + error)
     }
 }
